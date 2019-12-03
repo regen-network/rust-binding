1fff1bbc4aa32d67edff2ebcddf4cca22609e9f8
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-12-03 10:47:39 +0100

    Init project

---
+++ src/logic.rs
@@ -0,0 +1,25 @@
+// logic contains the actual rust code we want to test, not any bindings
+pub fn sum(a: u32, b: u32) -> u32 {
+    a + b
+}
+
+pub fn concat(a: &[u8], b: &[u8]) -> Vec<u8> {
+    [a, b].concat()
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn check_sum() {
+        assert_eq!(sum(2, 2), 4);
+    }
+
+    #[test]
+    fn check_concat() {
+        assert_eq!(concat(b"foo", b"bar"), b"foobar");
+    }
+
+}
