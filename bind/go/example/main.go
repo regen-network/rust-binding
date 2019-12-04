fd2d98ab2b7064b169eab5106db326f39b7197a1
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-12-04 19:31:09 +0100

    All swig binding examples work

--- bind/go/example/main.go
+++ bind/go/example/main.go
@@ -3,31 +3,31 @@ package main
 import (
 	"fmt"
 
-	rust_binding "sample.com/rust_binding/go"
+	rust_ffi "sample.com/rust_ffi/go"
 )
 
 func main() {
 	words := "how long is this?"
-	cnt := rust_binding.Count(words)
+	cnt := rust_ffi.Count(words)
 	fmt.Printf("Count('%s'): %d\n", words, cnt)
 
-	sum := rust_binding.Sum(7, 12)
+	sum := rust_ffi.Sum(7, 12)
 	fmt.Printf("Sum(7, 12): %d\n", sum)
 
-	concat := rust_binding.Concat("hello ", "world")
+	concat := rust_ffi.Concat("hello ", "world")
 	fmt.Printf("Concat('hello ', 'world'): %s\n", concat)
 
 	// one way to construct it (auto-gen)
-	foo := rust_binding.NewFoo()
+	foo := rust_ffi.NewFoo()
 	foo.SetCount(17)
 
 	// more explicit (from foo_new rust call)
-	foo = rust_binding.FooNew(17)
+	foo = rust_ffi.FooNew(17)
 
 	fmt.Printf("multiplier: %d\n", foo.GetCount())
-	fmt.Printf("foo_multiply(5): %d\n", rust_binding.FooMultiply(foo, 5))
+	fmt.Printf("foo_multiply(5): %d\n", rust_ffi.FooMultiply(foo, 5))
 
 	foo.SetCount(12)
 	fmt.Printf("multiplier: %d\n", foo.GetCount())
-	fmt.Printf("foo_multiply(6): %d\n", rust_binding.FooMultiply(foo, 6))
+	fmt.Printf("foo_multiply(6): %d\n", rust_ffi.FooMultiply(foo, 6))
 }
--- bind/go/force_link.go
+++ bind/go/force_link.go
@@ -1,8 +1,8 @@
-package rust_binding
+package rust_ffi
 
 // Note that we link to librust_bining.* in standard system path
 // -L${SRCDIR} in the following line would allow shipping with the .{so,dylib,dll}
 // in the go directory and not in the system path (since we need it for python, I use /usr/local/lib here to standarize)
 
-// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -lrust_binding
+// #cgo LDFLAGS: -Wl,-rpath,${SRCDIR} -lrust_ffi
 import "C"
