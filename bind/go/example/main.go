89e4b4157ed3b7697191083863496fd0f6484734
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-12-04 15:48:46 +0100

    Complete basic go example

--- bind/go/example/main.go
+++ bind/go/example/main.go
@@ -10,4 +10,18 @@ func main() {
 	words := "how long is this?"
 	cnt := rust_binding.Count(words)
 	fmt.Printf("Count('%s'): %d\n", words, cnt)
+
+	sum := rust_binding.Sum(7, 12)
+	fmt.Printf("Sum(7, 12): %d\n", sum)
+
+	concat := rust_binding.Concat("hello ", "world")
+	fmt.Printf("Concat('hello ', 'world'): %s\n", concat)
+
+	foo := rust_binding.Foo_new(17)
+	fmt.Printf("multiplier: %d\n", foo.GetCount())
+	fmt.Printf("foo_multiply(5): %d\n", rust_binding.Foo_multiply(foo, 5))
+
+	foo.SetCount(12)
+	fmt.Printf("multiplier: %d\n", foo.GetCount())
+	fmt.Printf("foo_multiply(6): %d\n", rust_binding.Foo_multiply(foo, 6))
 }
