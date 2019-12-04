d50004d36594a181bbb6c20370a4ec39ec20b2c3
Ethan Frey <ethanfrey@users.noreply.github.com>
2019-12-04 20:29:04 +0100

    Trial using structs in wasm_bindgen, note no &mut allowed

--- lib/rust-wasm/src/lib.rs
+++ lib/rust-wasm/src/lib.rs
@@ -22,3 +22,53 @@ pub fn count(word: &str) -> usize{
 pub fn concat(a: &str, b: &str) -> String {
     rust_binding::concat(a, b)
 }
+
+// Here we use custom wasm type
+// Other option is to include #[wasm_bindgen] next to #[repr(C)] in the upstream lib
+// Let's try it out
+
+#[wasm_bindgen]
+#[derive(Copy,Clone)]
+pub struct Foo {
+    pub count: i32,
+}
+
+impl Foo {
+    fn from_rust(foo: rust_binding::Foo) -> Self {
+        Foo{count: foo.count}
+    }
+
+    fn to_rust(&self) -> rust_binding::Foo {
+        rust_binding::Foo{count: self.count}
+    }
+}
+
+#[wasm_bindgen]
+pub fn foo_new(count: i32) -> Foo {
+    Foo::from_rust(rust_binding::new_foo(count))
+}
+
+#[wasm_bindgen]
+// note that we need to pass in &Foo as derive(Copy) doesn't help
+// not setting it causes foo to be freed
+pub fn foo_multiply(foo: &Foo, val: i32) -> i32 {
+    foo.to_rust().multiply(val)
+}
+
+// also note that we cannot accept &mut args, thus update is useless (uncomment and see compiler error)
+//#[wasm_bindgen]
+//pub fn foo_update(ptr: Option<&mut Foo>, val: i32) {
+//    match ptr {
+//        Some(foo) => foo.to_rust().update(val),
+//        None => {},
+//    };
+//}
+
+// work-around consuming one object and returning a new one...
+// here we don't need that, but assume there is another mutable operation
+#[wasm_bindgen]
+pub fn foo_update(old: Foo, val: i32) -> Foo {
+    let mut out = old.to_rust();
+    out.update(val);
+    Foo::from_rust(out)
+}
